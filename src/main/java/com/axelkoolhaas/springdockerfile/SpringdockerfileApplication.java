bfc5890c35180fae4957b0d7652db2a59b01cc8d
Axel Koolhaas <axel.koolhaas@outlook.com>
2021-09-01 16:50:21 +0200

    Fresh template from Spring initializr

---
+++ src/main/java/com/axelkoolhaas/springdockerfile/SpringdockerfileApplication.java
@@ -0,0 +1,13 @@
+package com.axelkoolhaas.springdockerfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdockerfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdockerfileApplication.class, args);
+	}
+
+}
