fcd62c2408f23b6e640ebff2b53e74d6ee5a749e
Axel Koolhaas <axel.koolhaas@outlook.com>
2021-09-01 16:57:06 +0200

    Added basic "/" map to respond to HTTP request

--- src/main/java/com/axelkoolhaas/springdockerfile/SpringdockerfileApplication.java
+++ src/main/java/com/axelkoolhaas/springdockerfile/SpringdockerfileApplication.java
@@ -2,9 +2,17 @@ package com.axelkoolhaas.springdockerfile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringdockerfileApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello there!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdockerfileApplication.class, args);
